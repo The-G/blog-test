698e4334c78ac449853ac7585e06e91cac90ad3a
Jihyuk Kim <jihyukkim@Jihyukui-MacBook-Pro.local>
2018-11-18 19:52:36 +0900

    Create multi model form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_071551) do
+ActiveRecord::Schema.define(version: 2018_11_18_103048) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "commenter"
@@ -29,5 +29,14 @@ ActiveRecord::Schema.define(version: 2018_11_18_071551) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_tags_on_post_id"
+  end
+
   add_foreign_key "comments", "posts"
+  add_foreign_key "tags", "posts"
 end

10877d1d96cd96f85b428c81ec90a4c4beb0c0e1
Jihyuk Kim <jihyukkim@Jihyukui-MacBook-Pro.local>
2018-11-18 20:42:50 +0900

    hotfix permit problem when use nested thing & practice view helper DONE

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -95,6 +95,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :title, :content)
+      params.require(:post).permit(:name, :title, :content, :tags_attributes => [:id, :name, :_destroy])
     end
 end
